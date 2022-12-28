6911908722fbf9285e24cd85dea6984a949a1284
redjen8 <seokwoo2525@gmail.com>
2022-12-28 15:53:58 +0900

    zipWith 테스트 추가

---
+++ webflux-streamapi-test/src/main/java/com/redjen/webfluxstreamapitest/WebfluxStreamapiTestApplication.java
@@ -0,0 +1,13 @@
+package com.redjen.webfluxstreamapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxStreamapiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxStreamapiTestApplication.class, args);
+    }
+
+}
---
+++ webflux-streamapi-test/src/test/java/com/redjen/webfluxstreamapitest/WebfluxStreamapiTestApplicationTests.java
@@ -0,0 +1,32 @@
+package com.redjen.webfluxstreamapitest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootTest
+class WebfluxStreamapiTestApplicationTests {
+
+    private final int[] intArr = { 1, 2, 3, 4, 5, 6, 7, 8};
+    private static List<Integer> intArrayList1, intArrayList2;
+
+    @BeforeAll
+    static void init() {
+        intArrayList1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+        intArrayList2 = new ArrayList<>(Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1));
+    }
+
+    @Test
+    void zip_with_test() {
+        Flux<Integer> flux1 = Flux.fromIterable(intArrayList1);
+        Flux<Integer> flux2 = Flux.fromIterable(intArrayList2);
+        flux1.zipWith(flux2).toIterable().forEach(System.out::println);
+    }
+
+}
