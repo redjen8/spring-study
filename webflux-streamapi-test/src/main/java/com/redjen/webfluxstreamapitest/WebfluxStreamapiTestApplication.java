ff2c43e9864b905948114b005412b7eca507fe2f
redjen8 <seokwoo2525@gmail.com>
2023-01-02 15:02:46 +0900

    test hello api 생성

--- webflux-streamapi-test/src/main/java/com/redjen/webfluxstreamapitest/WebfluxStreamapiTestApplication.java
+++ webflux-streamapi-test/src/main/java/com/redjen/webfluxstreamapitest/WebfluxStreamapiTestApplication.java
@@ -2,12 +2,22 @@ package com.redjen.webfluxstreamapitest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
+
+@RestController
 @SpringBootApplication
 public class WebfluxStreamapiTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebfluxStreamapiTestApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    Flux<String> hello() {
+        return Flux.just("Hello, world!");
     }
 
 }
