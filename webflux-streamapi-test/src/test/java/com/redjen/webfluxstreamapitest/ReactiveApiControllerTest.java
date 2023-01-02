a28461f27609999b2389d9a95f5b2d47d237f68d
redjen8 <seokwoo2525@gmail.com>
2023-01-02 23:36:18 +0900

    Flux<TestDomain> api 테스트 추가

--- webflux-streamapi-test/src/test/java/com/redjen/webfluxstreamapitest/ReactiveApiControllerTest.java
+++ webflux-streamapi-test/src/test/java/com/redjen/webfluxstreamapitest/ReactiveApiControllerTest.java
@@ -8,7 +8,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class ReactiveApiControllerTest {
@@ -35,5 +38,20 @@ public class ReactiveApiControllerTest {
             .expectNext("Hello")
             .expectNext("world!")
             .verifyComplete();
+    }
+
+    @Test
+    void testDomainApiTest() {
+        Flux<TestDomain> result = client.get()
+                .uri("/test")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .returnResult(TestDomain.class)
+                .getResponseBody();
+
+        StepVerifier.create(result)
+                .assertNext(t -> assertThat(t.getName()).isEqualTo("test1"))
+                .assertNext(t -> assertThat(t.getName()).isEqualTo("test2"))
+                .verifyComplete();
     }
 }
