0a4f12ebedcde37253bae02f6d1ed6403c1be1fe
redjen8 <seokwoo2525@gmail.com>
2023-01-02 16:53:13 +0900

    iterable한 stream 리턴하는 api 추가

--- webflux-streamapi-test/src/main/java/com/redjen/webfluxstreamapitest/WebfluxStreamapiTestApplication.java
+++ webflux-streamapi-test/src/main/java/com/redjen/webfluxstreamapitest/WebfluxStreamapiTestApplication.java
@@ -1,5 +1,9 @@
 package com.redjen.webfluxstreamapitest;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,4 +24,10 @@ public class WebfluxStreamapiTestApplication {
         return Flux.just("Hello", "world!");
     }
 
+    @GetMapping("/stream")
+    Flux<Map<String, Integer>> streamTest() {
+        Stream<Integer> intStream = Stream.iterate(1, i -> i+1).limit(100);
+        return Flux.fromStream(intStream)
+            .map(i -> Collections.singletonMap("value", i));
+    }
 }
